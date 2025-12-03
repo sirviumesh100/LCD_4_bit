49d605e2e1010ce8bf7e8b96639d412e359e8a45
sirviumesh100 <umesirvi500@gmail.com>
2025-12-03 17:48:24 +0530

    Clean up header comments in lcd.py
    Removed header comments and wiring instructions.

--- lcd.py
+++ lcd.py
@@ -1,32 +1,3 @@
-#!/usr/bin/python
-#
-# HD44780 LCD Test Script for
-# Raspberry Pi
-#
-# Author : Matt Hawkins
-# Site   : http://www.raspberrypi-spy.co.uk
-# 
-# Date   : 03/08/2012
-#
-
-# The wiring for the LCD is as follows:
-# 1 : GND
-# 2 : 5V
-# 3 : Contrast (0-5V)*
-# 4 : RS (Register Select)
-# 5 : R/W (Read Write)       - GROUND THIS PIN
-# 6 : Enable or Strobe
-# 7 : Data Bit 0             - NOT USED
-# 8 : Data Bit 1             - NOT USED
-# 9 : Data Bit 2             - NOT USED
-# 10: Data Bit 3             - NOT USED
-# 11: Data Bit 4
-# 12: Data Bit 5
-# 13: Data Bit 6
-# 14: Data Bit 7
-# 15: LCD Backlight +5V**
-# 16: LCD Backlight GND
-
 #import
 import RPi.GPIO as GPIO
 import time
